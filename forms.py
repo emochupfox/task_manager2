cca36a86f9c232a5727aae19e0ba8362d041554e
emochupfox <119287424+emochupfox@users.noreply.github.com>
2025-03-06 18:53:15 +0800

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Task
+
+class TaskCreateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date'] # Fields included in the create form
+
+class TaskUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'status']
